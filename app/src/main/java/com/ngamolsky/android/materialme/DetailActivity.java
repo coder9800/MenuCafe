8bc0f53cf279d0557b8e5d70f9bdc09ae1ad1c2f
fariz2445 <farizalfarisi80@gmail.com>
2017-10-30 16:32:15 +0700

    UTS Mobile Programming

---
+++ app/src/main/java/com/ngamolsky/android/materialme/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.ngamolsky.android.materialme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.materialme.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
